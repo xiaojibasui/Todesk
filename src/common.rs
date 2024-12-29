1959cf44e48fb08d2115ac2f4f0d0373435ce4cc
xiaojibasui <l22333@qq.com>
2024-12-29 19:35:11 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -909,12 +909,12 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://192.168.192.67:37242".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
     let url = get_api_server(api, custom);
-    if url.is_empty() || url.contains("rustdesk.com") {
+    if url.is_empty() || url.contains("192.168.192.67:8080") {
         return "".to_owned();
     }
     format!("{}/api/audit/{}", url, typ)
